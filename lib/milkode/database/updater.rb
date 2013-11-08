f438fec00bcdb540f2e4e7d9d218945e8c8f674d
Yoki Okawa <okawa@cross-cafe.me>
2013-11-08 11:23:47 +0800

    Follow symlink

--- lib/milkode/database/updater.rb
+++ lib/milkode/database/updater.rb
@@ -212,6 +212,8 @@ module Milkode
         case File.ftype(fpath)
         when "directory"
           searchDirectory(dirname, packname, next_path, depth + 1)
+        when "link"
+          searchDirectory(dirname, packname, next_path, depth + 1)
         when "file"
           unless ignoreFile?(fpath, next_path)
             db_add_file(dirname, next_path) # shortpathの先頭に'/'が付いているのが気になる
